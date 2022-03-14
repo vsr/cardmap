176f059ca66089ab0b57130f35b725d1a6130d72
Vinay Raikar <rockraikar@gmail.com>
2022-03-14 22:44:41 +0530

    some ux changes

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 import { reactive, computed, onMounted } from 'vue';
 import Card from "./Card.vue";
+import AddIcon from './AddIcon.vue';
+import CheckIcon from './CheckIcon.vue';
+import MinusIcon from './MinusIcon.vue';
+import ClearIcon from './ClearIcon.vue';
 
 export interface CardInterface {
   text: String;
@@ -8,20 +12,58 @@ export interface CardInterface {
   tags: String[];
 }
 const LS_CARD = "CARD_DATA";
-const data: {cards: CardInterface[], tagSelection: Set<String>} = reactive({cards: [], tagSelection: new Set()});
+const colors = [
+  "bg-red-600",
+  "bg-orange-600",
+  "bg-amber-600",
+  "bg-yellow-600",
+  "bg-lime-600",
+  "bg-green-600",
+  "bg-teal-600",
+  "bg-cyan-600",
+  "bg-sky-600",
+  "bg-blue-600",
+  "bg-indigo-600",
+  "bg-violet-600",
+  "bg-purple-600",
+  "bg-fuchisia-600",
+  "bg-pink-600",
+  "bg-rose-600"
+];
+
+
+const data: { cards: CardInterface[], tagSelection: Set<String>, tagColorMap: { [key: string]: string }, } = reactive({
+  cards: [],
+  tagSelection: new Set(),
+  tagColorMap: {},
+});
+const nextColor = () => colors[Math.floor(Math.random() * colors.length)];;
+
 const tags = computed(() => {
   const tagSet: Set<String> = new Set();
   data.cards.forEach(card => card.tags && card.tags.forEach(tag => tagSet.add(tag)));
-  return Array.from(tagSet) as String[];
+  const tagsList = Array.from(tagSet) as String[];
+  tagsList.forEach((tag) => {
+    if (!data.tagColorMap[tag as string]) {
+      data.tagColorMap[tag as string] = nextColor();
+    }
+  })
+  return tagsList;
 });
+
+
+
+
 const filteredCards = computed(() => {
-  if(data.tagSelection.size === 0) return data.cards;
+  if (data.tagSelection.size === 0) return data.cards;
   return data.cards.filter(card => {
-    let filter = false;
-    if(card.tags) {
-      card.tags.forEach(tag => {
-        if(data.tagSelection.has(tag)) filter = true;
-      });
+    let filter = true;
+    if (card.tags) {
+      data.tagSelection.forEach(tag => {
+        if (!card.tags.includes(tag)) {
+          filter = false;
+        }
+      })
     }
     return filter;
   })
@@ -29,14 +71,14 @@ const filteredCards = computed(() => {
 
 
 onMounted(() => {
-    try {
-      const lsData = window.localStorage.getItem(LS_CARD);
-      if (lsData) {
-        data.cards = JSON.parse(lsData).cards;
-      }
-    } catch (err) {
-      console.error("Error loading data", err);
+  try {
+    const lsData = window.localStorage.getItem(LS_CARD);
+    if (lsData) {
+      data.cards = JSON.parse(lsData).cards;
     }
+  } catch (err) {
+    console.error("Error loading data", err);
+  }
 });
 
 const save = () => window.localStorage.setItem(LS_CARD, JSON.stringify(data));
@@ -44,7 +86,7 @@ const save = () => window.localStorage.setItem(LS_CARD, JSON.stringify(data));
 const onCardUpdate = (cardData: CardInterface) => {
   const index = data.cards.findIndex((card) => card.id === cardData.id);
   if (index !== -1) {
-    data.cards[index] = {...data.cards[index], ...cardData };
+    data.cards[index] = { ...data.cards[index], ...cardData };
   }
   save();
 };
@@ -58,41 +100,71 @@ const onCardDelete = (cardData: CardInterface) => {
 };
 
 
-const add = () => data.cards.push({text: "", tags: [], id: Date.now().toString() });
+const add = () => data.cards.push({ text: "", tags: [], id: Date.now().toString() });
 
-const toggleTagSelection = (tag:String) => {
-  if(data.tagSelection.has(tag)) {
+const toggleTagSelection = (tag: String) => {
+  if (data.tagSelection.has(tag)) {
     data.tagSelection.delete(tag);
   } else {
     data.tagSelection.add(tag);
   }
 };
-const isTagSelected = (tag:String) => data.tagSelection.has(tag);
+const isTagSelected = (tag: String) => data.tagSelection.has(tag);
 
 const clearTagSelection = () => data.tagSelection.clear();
 </script>
 
 <template>
-  <div class="w-screen h-screen relative bg-slate-100">
-    <ul role="list" class="flex flex-wrap content-start" aria-label="Cards">
-      <Card v-for="card in filteredCards"
-      :onUpdate="onCardUpdate"
-      :onCardDelete="onCardDelete"
-      class="card "
-      :id="card.id" :card="card"></Card>
-     </ul>
-     <div class="fixed bottom-0 w-full bg-white p-2 text-sm flex ">
-       <div class="flex-grow overflow-x-auto">
-         <div class="flex gap-1">
-           <span class="p-1 rounded border cursor-pointer select-none"
-           :class="{'bg-indigo-700 text-indigo-50': isTagSelected(tag)}"
-            @click="toggleTagSelection(tag)" v-for="tag in tags">{{tag}}</span>
-         </div>
-       </div>
-       <div class="flex gap-1">
-         <button class="p-2 rounded bg-indigo-700 text-slate-100" @click="clearTagSelection">View All</button>
-         <button class="p-2 rounded bg-indigo-700 text-slate-100" @click="add">Add</button>
-       </div>
-     </div>
+  <div class="w-screen h-screen relative bg-slate-100 flex flex-col">
+    <ul
+      role="list"
+      class="flex flex-wrap content-start flex-grow overflow-y-auto pb-16"
+      aria-label="Cards"
+    >
+      <Card
+        v-for="card in filteredCards"
+        :onUpdate="onCardUpdate"
+        :onCardDelete="onCardDelete"
+        :tagColorMap="data.tagColorMap"
+        class="card"
+        :id="card.id"
+        :card="card"
+      ></Card>
+    </ul>
+    <div class="fixed bottom-0 w-full bg-white max-h-[4em] p-0 text-sm flex shadow-xl">
+      <div class="flex-grow flex self-center pr-2 border-r border-slate-300">
+        <ul class="p-2 overflow-x-auto flex-grow" role="list" aria-label="Filter by Tags">
+          <li class="flex gap-1" role="item">
+            <button
+              class="p-1 rounded border cursor-pointer select-none bg-slate-600 text-slate-50 flex"
+              role="button"
+              tabindex="0"
+              :class="{ 'ring-2 ring-slate-600': isTagSelected(tag), [`${data.tagColorMap[tag as string]}`]: true }"
+              @click="toggleTagSelection(tag)"
+              v-for="tag in tags"
+            >
+              <CheckIcon class="mr-1 self-center" v-if="isTagSelected(tag)" />
+              <MinusIcon class="mr-1 self-center" v-if="!isTagSelected(tag)" />
+              {{ tag }}
+            </button>
+          </li>
+        </ul>
+        <button
+          class="p-1 self-center text-sm rounded cursor-pointer select-none bg-transparent text-slate-600 flex"
+          @click="clearTagSelection"
+          aria-label="Clear Tag filter"
+        >
+          <ClearIcon />
+        </button>
+      </div>
+      <div class="flex p-2 justify-end gap-1 min-w-[10em] drop-shadow">
+        <button
+          class="inline-flex items-center px-2 py-1 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
+          @click="add"
+        >
+          <AddIcon role="presentation" class="mr-1" />Add Card
+        </button>
+      </div>
+    </div>
   </div>
 </template>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { reactive, computed, ref, nextTick } from 'vue';
+import DeleteIcon from './DeleteIcon.vue';
 
 interface CardInterface {
   text: String;
@@ -15,6 +16,7 @@ const props = defineProps({
   card: { type: Object, required: true },
   onUpdate: {type: Function, required: true},
   onCardDelete: {type: Function, required: true},
+  tagColorMap: {type: Object, required: true}
 });
 
 const state: {
@@ -48,7 +50,7 @@ const tagsEdit = (editable:boolean) => {
     } else {
             if(tagsEl.value) {
                 const el = tagsEl.value as HTMLInputElement;
-                const tags = el.value.split(",").map(tag => tag.trim());
+                const tags = Array.from(new Set(el.value.split(",").map(tag => tag.trim()).filter(tag => tag)));
                 props.onUpdate({...props.card, tags })
             }
             focusCard();
@@ -108,10 +110,12 @@ const deleteCard = () => {
 <div class="relative group overflow-hidden flex flex-col focus:border-indigo-600 select-none min-h-[4em] min-w-[12em] max-w-[24em] max-h-[24em]  place-content-center items-center p-1 m-2   duration-75 ease-linear transition-transform bg-white rounded-md shadow " tabindex="0"
     ref="cardEl" :aria-label="props.card.text" role="list"
     @keypress="cardKeypress">
-    <button @click.stop="deleteCard"
-         class="absolute right-0 top-0 px-2 py-1 font-bold bg-pink-300 text-slate-200 focus:bg-pink-500  text-xs opacity-0 group-hover:opacity-100 group-focus:opacity-100 hover:opacity-100 focus:opacity-100 outline-none">delete</button>
+    <button @click.stop="deleteCard" aria-label="Delete card"
+         class="absolute right-0 top-0 p-1 rounded-full font-bold bg-red-500 text-slate-50 focus:bg-red-600  text-xs opacity-0 group-hover:opacity-100 group-focus:opacity-100 hover:opacity-100 focus:opacity-100 outline-none focus:border-red-800">
+         <DeleteIcon />
+         </button>
 
-    <span class="my-3 text-xl w-full min-h-[2em] bg-white flex place-content-center items-center overflow-x-hidden border-b border-slate-200"
+    <span class="my-2 py-3 text-xl w-full min-h-[2em] bg-white flex place-content-center items-center overflow-x-hidden border-b border-slate-200"
         @dblclick="edit(true)"
         @focusout="edit(false)"
         :contenteditable="state.editable"
@@ -125,9 +129,11 @@ const deleteCard = () => {
         @dblclick="tagsEdit(true)"
         @touchend="tagTap"
         >
-        <span class="px-2 py-1 bg-slate-800 text-slate-50 inline-flex rounded border shadow-sm" v-if="props.card.tags && !state.tagsEditing" v-for="tag in props.card.tags">{{tag}}</span>
+        <span class="px-2 py-1 bg-slate-800 text-slate-50 inline-flex rounded border shadow-sm"
+            :class="{[`${props.tagColorMap[tag as string]}`]: true}"
+            v-if="props.card.tags && !state.tagsEditing" v-for="tag in props.card.tags">{{tag}}</span>
         <input
-            class="shadow-sm text-md font-mono p-1 ring-2 ring-indigo-600 outline-none block w-full sm:text-sm border-gray-300 rounded-sm"
+            class="shadow-sm text-md font-mono p-1 ring-2 ring-indigo-600 outline-none bg-neutral-50 block w-full sm:text-sm border-gray-300 rounded-sm"
             placeholder="Enter comma separated words for tags"
             @focusout="tagsEdit(false)"
             @keypress.stop="noop"
@@ -139,6 +145,6 @@ const deleteCard = () => {
 </template>
 <style scoped>
 [contenteditable="true"] {
-    @apply rounded-sm bg-yellow-50 outline-none;
+    @apply rounded-sm bg-neutral-50 outline-none;
 }
 </style>
